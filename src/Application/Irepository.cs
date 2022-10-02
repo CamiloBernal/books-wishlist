6e197c816227561e3d2909878f1225e94cd62a98
Camilo Bernal <me@camilobernal.dev>
2022-10-02 09:31:38 -0500

    Remove mandatory validation for user email

--- src/Application/Irepository.cs
+++
@@ -1,9 +0,0 @@
-public interface IRepository<T> where T : class, new()
-{
-    Task<T> Create(T entity);
-    Task<IEnumerable<T>> Read();
-
-    Task<T> Update(T entity);
-
-    Task<T> Delete(T entity);
-}
